e84c3b0cabae3efa5c536d38a21541e29482e2e5
Andrew <andrew.vinocur@gmail.com>
2018-06-05 16:06:42 -0700

    fixes misplaced file

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-
   render() {
     return (
-      <div></div>
+      <div>Hello</div>
     );
   }
-
 }
 
 export default App;
