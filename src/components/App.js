67055d4d2199faf2aa0842d4982cbddb36cd95b6
Andrew <andrew.vinocur@gmail.com>
2018-06-05 22:53:44 -0700

    adds signup route

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { Route, Link } from 'react-router-dom';
 
 import Login from './Login';
 import Landing from './Landing';
+import Signup from './Signup';
 import header from '../styles/header.css';
 
 class App extends Component {
@@ -25,6 +26,7 @@ class App extends Component {
     return (
       <div>
         {this.renderHeader()}
+        <Route path="/signup" component={Signup} />
         <Route path="/login" component={Login} />
         <Route path="/" exact component={Landing} />
       </div>
