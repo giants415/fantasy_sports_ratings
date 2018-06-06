cd97428d7a09b0ccd98dbc858b683f49d4872275
Andrew <andrew.vinocur@gmail.com>
2018-06-05 17:36:18 -0700

    creates landing

---
+++ src/components/Landing.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+
+  render() {
+    return (
+      <div>
+        <p>
+          Welcome to uRate, the premier place for fantasy sports players to
+          compare and keep track of rest of season rankings!
+        </p>
+        <button>Sign Up</button>
+      </div>
+    );
+  }
+
+}
+
+export default Landing;
