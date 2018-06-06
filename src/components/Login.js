60e4b5e3e46fc5b6004d4d78a6a562d050bbb356
Andrew <andrew.vinocur@gmail.com>
2018-06-05 17:31:50 -0700

    creates log in page

---
+++ src/components/Login.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+
+  render() {
+    return (
+      <div>
+        <div>
+          <input type='text' label='username'>
+          <input type='text' label='password'>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default Login;
