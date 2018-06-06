8e16e8c6682eeb0acc06e6f034b1ef7676752d12
Andrew <andrew.vinocur@gmail.com>
2018-06-05 22:49:41 -0700

    adds link for sign up

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Landing extends Component {
 
@@ -9,7 +10,9 @@ class Landing extends Component {
           Welcome to uRate, the premier place for fantasy sports players to
           compare and keep track of rest of season rankings!
         </p>
-        <button>Sign Up</button>
+        <button>
+          <Link to="/signup">Sign Up</Link>
+        </button>
       </div>
     );
   }
