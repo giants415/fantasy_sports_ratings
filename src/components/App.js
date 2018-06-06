2f233822233bcc1a8c8718e8ca8a63c96c97f8ee
Andrew <andrew.vinocur@gmail.com>
2018-06-05 17:37:23 -0700

    refactor to use Link and routes for login

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 
+import Login from './Login';
+import Landing from './Landing';
 import header from '../styles/header.css';
 
 class App extends Component {
   renderHeader() {
     return (
       <ul ClassName="header">
-        <li>Home</li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
         <li>Fantasy Rankings</li>
         <li>
           <Link to="/login">Log In</Link>
@@ -21,7 +25,7 @@ class App extends Component {
     return (
       <div>
         {this.renderHeader()}
-
+        <Route path="/login" component={Login} />
       </div>
     );
   }
