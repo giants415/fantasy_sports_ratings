cfb9c9b30cedf3a72ede51bc5e1a88caf8b6b8bd
Andrew <andrew.vinocur@gmail.com>
2018-06-07 09:10:34 -0700

    implements normalize

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import Normalize from 'normalize.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
@@ -5,7 +6,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import App from './components/App';
 
 ReactDOM.render(
-  <BrowserRouter className="container-fluid">
+  <BrowserRouter className="Normalize container-fluid">
     <Route path="/" component={App} />
   </BrowserRouter>
   , document.getElementById('root')
