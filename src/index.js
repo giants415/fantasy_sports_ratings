b80d6a3ad7cf3d955b206e575b357ba399df7329
Andrew <andrew.vinocur@gmail.com>
2018-06-26 12:42:22 -0700

    refactors for redux

--- src/index.js
+++ src/index.js
@@ -2,12 +2,23 @@ import Normalize from 'normalize.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
+import reducers from './reducers';
 import App from './components/App';
 
+const store = createStore(
+  reducers,
+  {},
+  applyMiddleware()
+);
+
 ReactDOM.render(
-  <BrowserRouter className="Normalize container-fluid">
-    <Route path="/" component={App} />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter className="Normalize container-fluid">
+      <Route path="/" component={App} />
+    </BrowserRouter>
+  </Provider>
   , document.getElementById('root')
 );
